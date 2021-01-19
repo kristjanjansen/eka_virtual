3b4cc83b2e34bf4aba6ab1d82c782697b67d382e
kristjanjansen <kristjan.jansen@gmail.com>
2021-01-19 22:09:10 +0200

    cleanup

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -1,4 +1,4 @@
-import { computed } from "../deps/vue.js";
+import { ref, computed } from "../deps/vue.js";
 import { Select } from "./index.js";
 
 export default {
@@ -7,7 +7,6 @@ export default {
     cameraIndex: { type: Number, default: 0 },
     micIndex: { type: Number, default: 0 },
   },
-  emits: ["update:cameraIndex", "update:micIndex"],
   setup(props, { emit }) {
     const camera = computed({
       get: () => props.cameraIndex,
@@ -25,18 +24,55 @@ export default {
       },
     });
 
-    return { camera, mic };
+    const cameraOptions = ref([]);
+    const micOptions = ref([]);
+    const video = ref(null);
+
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: true })
+      .then((stream) => {
+        video.value.srcObject = stream;
+        video.value.onloadedmetadata = () => video.value.play();
+
+        navigator.mediaDevices.enumerateDevices().then((dev) => {
+          cameraOptions.value = Object.fromEntries(
+            dev
+              .filter(({ kind }) => kind === "videoinput")
+              .map(({ label }, index) => [index, label])
+          );
+
+          micOptions.value = Object.fromEntries(
+            dev
+              .filter(({ kind }) => kind === "audioinput")
+              .map(({ label }, index) => [index, label])
+          );
+        });
+      });
+
+    const onStart = () => emit("start");
+
+    return { video, camera, cameraOptions, mic, micOptions, onStart };
   },
   template: `
-  <div class="overlay">
+  <div class="overlay" style="background: rgba(0,0,0,0.9);">
     <div class="modal" style="display: grid; gap: 16px;">
-      <div style="display: grid; gap: 16px; grid-template-columns: 1fr 1fr">
+      <video
+        style="
+          clipPath: circle(40%);
+          width: 320px;
+          transform: scale(-1,1);
+        "
+        ref="video"
+        autoplay
+        muted
+      />
+      <div style="display: grid; gap: 16px; grid-template-columns: auto auto;">
         <label>Select camera</label>
-        <Select v-model="camera" :options="{0: 'Camera1', 1: 'Camera2'}" />
+        <Select v-model="camera" :options="cameraOptions" />
         <label>Select mic</label>
-        <Select v-model="mic" :options="{0: 'Mic1', 1: 'Mic2'}" />
+        <Select v-model="mic" :options="micOptions" />
       </div>
-      <button>Start</button>
+      <button @click="onStart">Start</button>
     </div>
   </div>
   `,
