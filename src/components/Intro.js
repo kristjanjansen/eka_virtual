c68c33c52f73113bdd8976300a7094f6e85cac43
Kristjan Jansen <kristjan.jansen@gmail.com>
2021-01-22 14:30:26 +0200

    Readme (#27)

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -21,7 +21,7 @@ export default {
       <h1 style="margin: 0">Welcome to<br />EKA Virtual!</h1>
       <div style="line-height: 1.5em;">Click and drag your avatar to move around and join groups to work in teams</div>
       <button class="primary" @click="emit('submit')">Set up audio and video</button>
-      <div style="line-height: 1.5em; opacity: 0.5">The project was implemented by Estonian <a href="https://www.artun.ee/en/home">Academy of Arts</a> digital product design students at Jan 2021. Source code is <a href="https://github.com/kristjanjansen/eka_virtual">available here</a>.</div>
+      <div style="line-height: 1.5em; opacity: 0.5">The project was implemented by Estonian <a href="https://www.artun.ee/en/home">Academy of Arts</a> digital product design students in Jan 2021. Source code is <a href="https://github.com/kristjanjansen/eka_virtual">available here</a>.</div>
     </div>
   </div>
   `,
